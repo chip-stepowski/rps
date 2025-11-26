0046f071443b42802dda0a2c98858cb6c9eecedc
Christopher Stepowski <christopher.stepowski@hashicorp.com>
2025-11-26 14:00:36 -0500

    added user choice options

--- python-rps.py
+++ python-rps.py
@@ -3,3 +3,21 @@
 # import random as we will need it to make the computer's choice
 import random
 
+print("Welcome to Rock, Paper, Scissors!")
+
+wins = 0
+losses = 0
+ties = 0
+
+playing = True
+
+while playing:
+    print("Wins: " + str(wins))
+    print("Losses: " + str(losses))
+    print("Ties: " + str(ties))
+
+    user_choice = input("Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit: ")
+
+    print('Your choice: ' + user_choice)
+
+    
