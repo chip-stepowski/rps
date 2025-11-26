e10a685811a1d17151bced4ed16df63ca9848fe0
Christopher Stepowski <christopher.stepowski@hashicorp.com>
2025-11-26 13:46:44 -0500

    first commit

---
+++ python-rps.py
@@ -0,0 +1,5 @@
+# setup a rock paper scissors game with a computer opponent, and a wins, losses, and ties counter
+
+# import random as we will need it to make the computer's choice
+import random
+
